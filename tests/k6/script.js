faa69ef085577f547cd217bc2c53402ca7bfbf16
koduki <pascalm3+git@gmail.com>
2025-09-21 00:20:29 +0900

    Add Multi Containers

--- tests/k6/script.js
+++ tests/k6/script.js
@@ -2,6 +2,6 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 export default function () {
-  http.get('https://example-dotnet-app-367762831588.asia-northeast1.run.app/api/TodoItems');
+  http.get('https://example-dotnet-367762831588.asia-northeast1.run.app/api/TodoItems');
   sleep(1);
 }
