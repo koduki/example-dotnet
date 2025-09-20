3069b99e2c44da2d1540a12b0aa1fec0fdde5fd2
koduki <pascalm3+git@gmail.com>
2025-09-20 20:28:54 +0900

    Add Cloud Trace

--- Program.cs
+++ Program.cs
@@ -1,14 +1,34 @@
 using Microsoft.EntityFrameworkCore;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 using TodoApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // DIへのサービスの登録
-builder.Services.AddDbContext<TodoContext>(opt =>
-    opt.UseInMemoryDatabase("TodoList"));
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddOpenApi();
+builder.Services.AddOpenTelemetry()
+    .ConfigureResource(resource => resource.AddService(builder.Environment.ApplicationName))
+    .WithTracing(tracing =>
+    {
+        tracing
+            .AddAspNetCoreInstrumentation()
+            .AddEntityFrameworkCoreInstrumentation(options =>
+            {
+                options.SetDbStatementForText = true;
+            })
+            .AddOtlpExporter(); 
+
+        // 開発環境でのみ、コンソールにもトレースを出力する
+        if (builder.Environment.IsDevelopment())
+        {
+            tracing.AddConsoleExporter();
+        }
+    });
+builder.Services.AddDbContext<TodoContext>(opt =>
+    opt.UseInMemoryDatabase("TodoList"));
 
 // Appの作成
 var app = builder.Build();
